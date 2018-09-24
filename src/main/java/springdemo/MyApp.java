bde7e4253ceee7961faecf32eb810c1b721f5f73
Abbasoglu <babbasoglu@trbabbasoglumac.atrema.deloitte.com>
2018-09-24 11:05:38 +0300

    first commit

---
+++ src/main/java/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package springdemo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        Coach theCoach = new TrackCoach();
+
+        System.out.print(theCoach.getDailyWorkout());
+
+    }
+
+}
