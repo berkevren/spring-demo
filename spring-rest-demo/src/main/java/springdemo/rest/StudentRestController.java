ce197bb96ff2f3665e15d872bb75404e008fb1d6
berkevren <berkabbasoglu@hotmail.com>
2018-11-20 13:33:53 +0300

    (Global) Exception Handling

--- spring-rest-demo/src/main/java/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/springdemo/rest/StudentRestController.java
@@ -1,9 +1,8 @@
 package springdemo.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import springdemo.entity.Student;
 
 import javax.annotation.PostConstruct;
@@ -34,10 +33,10 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
 
-        Student student = students.get(studentId);
-        return student;
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return students.get(studentId);
     }
-    
-
-
 }
